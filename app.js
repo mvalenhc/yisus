30822501622439445146af55f77a770f20955d6b
santiRendon <srendon34@soy.sena.edu.co>
2023-02-17 17:14:27 -0500

    add comment in app.js, router.js and all ejs views

--- app.js
+++ app.js
@@ -2,15 +2,16 @@ const express = require("express");
 const app = express();
 const dotenv = require('dotenv').config();
 
-// Static Files
+//* Static Files
 app.use("/assets", express.static(__dirname + "/public"));
 
-//4- Estableciendo el motor de plantillas
+//* Estableciendo el motor de plantillas
 app.set("view engine", "ejs");
 
-//Establecemos las rutas para las VISTAS usando un archivo aparte (router.js) y la clase Router()
+//* Establecemos las rutas para las VISTAS usando un archivo aparte (router.js) y la clase Router()
 app.use("/", require("./router"));
 
+//* Servidor 
 app.listen(process.env.PORT, () => {
   console.log(`Server in http://localhost:${process.env.PORT}`);
 });
--- router.js
+++ router.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
+// vista principal de la pagina
 router.get('/', (req,res)=>{
     res.render('index.ejs');
 })
 
+// vista del login o inicio de sesion
 router.get('/login',(req,res)=>{
     res.render('login.ejs')
 })
 
+// vista autentificacion de prestamo
 router.get('/auth',(req,res)=>{
     res.render('auth.ejs')
 })
 
+// Ruta por defecto al ingresar una ruta desconocida
 router.use((req,res)=>{
     res.send(`<h1> Page not found </h1>`);
 })
