4fe6a7d3141f058cece9b90052ff84c0d938d3ba
SantiRendon <srendon34@soy.sena.edu.co>
2023-05-23 15:52:35 -0500

    Clean master

--- router.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// vista principal de la pagina
-router.get('/', (req,res)=>{
-    res.render('index.ejs');
-});
-
-module.exports = router;
