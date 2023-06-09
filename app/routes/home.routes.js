d1baadc0ff0d1e9ce06c12f5e980efeecc6ecb6a
SantiRendon <srendon34@soy.sena.edu.co>
2023-06-08 22:29:51 -0500

    prueba de render de imagenes y consumo de datos de los libros

--- app/routes/home.routes.js
+++ app/routes/home.routes.js
@@ -6,6 +6,24 @@ home.get('/', (req, res) => {
   res.render('index');
 });
 
+// Vista principal de la pagina
+home.get('/prueba', async (req, res) => {
+  let url = "http://localhost:3000/api/books/";
+  let options = { method: 'GET' };
+  let datosLibro = {};
+
+  let result = await fetch(url, options)
+    .then((response) => response.json())
+    .then((data) => {
+      datosLibro = data;
+    })
+    .catch(err => console.error("Error en peticion" + err))
+
+  // console.log(datosLibro);
+
+  res.render('prueba', {libros: datosLibro});
+});
+
 // vista para la pagina por genero
 home.get('/genero', (req, res) => {
   res.render('pagina-genero.ejs')
