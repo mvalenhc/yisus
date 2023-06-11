c55819a3999252edfbe4cd3a8a050dd2f06ffd22
SantiRendon <srendon34@soy.sena.edu.co>
2023-06-10 20:09:31 -0500

    Consumo de datos de libros añadido al index, imagenes jpeg funcionando... faltan png

--- app/routes/home.routes.js
+++ app/routes/home.routes.js
@@ -1,27 +1,42 @@
 import { Router } from 'express'
+import { Buffer } from 'node:buffer'
 const home = Router()
 
 // Vista principal de la pagina
-home.get('/', (req, res) => {
-  res.render('index');
-});
-
-// Vista principal de la pagina
-home.get('/prueba', async (req, res) => {
+home.get('/', async (req, res) => {
   let url = "http://localhost:3000/api/books/";
   let options = { method: 'GET' };
   let datosLibro = {};
 
-  let result = await fetch(url, options)
+  await fetch(url, options)
     .then((response) => response.json())
     .then((data) => {
-      datosLibro = data;
+
+      let convert = data.map(element => {
+        // Almacenamos el objeto que contiene el buffer de las portadas en una variable
+        let datosImagen = element.IMAGEN;
+
+        // Si datosImagen contiene datos dentro, convertimos el objeto a buffer y luego a base64
+        let buffer = datosImagen ? Buffer.from(datosImagen).toString("base64") : null
+
+        return {
+          "COD_LIBRO": element.COD_LIBRO,
+          "SIPNOPSIS": element.SIPNOPSIS,
+          "TITULO": element.TITULO,
+          "FECHA_PUBLICACION": element.FECHA_PUBLICACION,
+          "NUM_SERIE": element.NUM_SERIE,
+          "COD_GENERO": element.COD_GENERO,
+          "COD_AUTOR": element.COD_AUTOR,
+          "IMAGEN": buffer
+        };
+      });
+
+      datosLibro = convert;
+
     })
     .catch(err => console.error("Error en peticion" + err))
 
-  // console.log(datosLibro);
-
-  res.render('prueba', {libros: datosLibro});
+  res.render('index', { libros: datosLibro });
 });
 
 // vista para la pagina por genero
