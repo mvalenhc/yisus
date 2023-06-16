638728720b7e110847d71d4c30176c0eeec790dc
SantiRendon <srendon34@soy.sena.edu.co>
2023-06-16 01:05:17 -0500

    solucionado el problema de promises en renderizado de imagenes de libros... falta validar imagenes que NO sean jpeg

--- app/controllers/home.controller.js
+++ app/controllers/home.controller.js
@@ -10,8 +10,8 @@ const index = async (req, res) => {
 
     await fetch(url, options)
       .then((response) => response.json())
-      .then((data) => {
-        let convert = data.map(async (element) => {
+      .then(async (data) => {
+        let convert = await Promise.all(data.map(async (element) => {
           // Almacenamos el objeto que contiene el buffer de las portadas en una variable
           let datosImagen = element.IMAGEN;
 
@@ -19,8 +19,7 @@ const index = async (req, res) => {
           let buffer = datosImagen ? Buffer.from(datosImagen) : null;
           // Si buffer no está vacio, se saca el mimetype con la funcion fileTypeFromBuffer();
           let mimeType = buffer ? await fileTypeFromBuffer(buffer) : null;
-          console.log(mimeType);
-          // let mimeType = null;
+          // console.log(mimeType);
 
           return {
             COD_LIBRO: element.COD_LIBRO,
@@ -30,12 +29,10 @@ const index = async (req, res) => {
             NUM_SERIE: element.NUM_SERIE,
             COD_GENERO: element.COD_GENERO,
             COD_AUTOR: element.COD_AUTOR,
-            // IMAGEN: buffer ? { "base64": buffer.toString("base64")} : null
             IMAGEN: buffer ? { base64: buffer.toString("base64"), mimetype: mimeType } : null,
           };
-        });
+        }));
 
-        // console.log(convert);
         datosLibro = convert;
         res.render("index", { libros: datosLibro });
       });
